3df89c97c485b618d818af2dae116552f664f5f9
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-12-03 22:33:23 -0500

    added workspace and CorePedido

---
+++ CorePedido/CorePedido/CorePedido.h
@@ -0,0 +1,19 @@
+//
+//  CorePedido.h
+//  CorePedido
+//
+//  Created by Alsey Coleman Miller on 12/3/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CorePedido.
+FOUNDATION_EXPORT double CorePedidoVersionNumber;
+
+//! Project version string for CorePedido.
+FOUNDATION_EXPORT const unsigned char CorePedidoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CorePedido/PublicHeader.h>
+
+
