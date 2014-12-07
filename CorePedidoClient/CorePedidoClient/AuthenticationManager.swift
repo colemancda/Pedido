843273236d3a8940967afd0edb22cf82a3365418
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-12-07 10:30:14 -0500

    implemented basic AuthenticationManager

--- CorePedidoClient/CorePedidoClient/AuthenticationManager.swift
+++ CorePedidoClient/CorePedidoClient/AuthenticationManager.swift
@@ -8,7 +8,61 @@
 
 import Foundation
 
-public class AuthenticationManager {
+public class AuthenticationManager: StoreDelegate {
     
+    // MARK: - Properties
     
+    /** The store this manager will provide tokens to. Ensures that tokens can only be requested by one object. */
+    public let store: Store
+    
+    // MARK: - Private Properties
+    
+    private let tokenOperationQueue: NSOperationQueue = {
+        
+        let operationQueue = NSOperationQueue()
+        
+        operationQueue.name = "AuthenticationManager Token Operation Queue"
+        
+        operationQueue.maxConcurrentOperationCount = 1
+        
+        return operationQueue
+    }()
+    
+    private var token: String?
+    
+    // MARK: - Initialization
+    
+    public init(store: Store) {
+        
+        self.store = store
+    }
+    
+    // MARK: - StoreDelegate
+    
+    public func tokenForStore(store: Store) -> String? {
+        
+        assert(store === self.store, "Only the manager's store can call the delegate methods")
+        
+        var token: String?
+        
+        self.tokenOperationQueue.addOperations([NSBlockOperation(block: { () -> Void in
+            
+            token = self.token
+            
+        })], waitUntilFinished: true)
+        
+        return token
+    }
+    
+    public func store(store: Store, didLoginWithUsername username: String, password: String, token: String) {
+        
+        assert(store === self.store, "Only the manager's store can call the delegate methods")
+        
+        self.tokenOperationQueue.addOperationWithBlock { () -> Void in
+            
+            self.token = token
+        }
+    }
 }
+
+
