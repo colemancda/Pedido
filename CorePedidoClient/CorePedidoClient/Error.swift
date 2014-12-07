b39885e470b8f628b660d6ad8e2c1566090ecfaa
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-12-07 00:27:17 -0500

    finished implementing Store -loginWithUsername:

---
+++ CorePedidoClient/CorePedidoClient/Error.swift
@@ -0,0 +1,12 @@
+//
+//  Error.swift
+//  CorePedidoClient
+//
+//  Created by Alsey Coleman Miller on 12/7/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+import Foundation
+
+/** The reverse DNS error domain string for NetworkObjects. */
+public let CorePedidoClientErrorDomain = "com.colemancda.CorePedidoClient.ErrorDomain"
--- CorePedidoClient/CorePedidoClient/Store.swift
+++ CorePedidoClient/CorePedidoClient/Store.swift
@@ -46,10 +46,73 @@ public class Store: NetworkObjects.Store {
         
         request.HTTPBody = NSJSONSerialization.dataWithJSONObject(["username": username, "password": password], options: NSJSONWritingOptions.allZeros, error: nil)
         
-        return URLSession.dataTaskWithRequest(request, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+        let dataTask = URLSession.dataTaskWithRequest(request, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
             
+            if error != nil {
+                
+                completionBlock(error: error, token: nil)
+            }
             
+            let httpResponse = response as NSHTTPURLResponse
+            
+            // error codes
+            
+            if httpResponse.statusCode != ServerStatusCode.OK.rawValue {
+                
+                let errorCode = ErrorCode(rawValue: httpResponse.statusCode)
+                
+                if errorCode != nil {
+                    
+                    if errorCode == ErrorCode.ServerStatusCodeForbidden {
+                        
+                        let frameworkBundle = NSBundle(identifier: "com.colemancda.CorePedidoClient")
+                        let tableName = "Error"
+                        let comment = "Description for ErrorCode.\(self) for Login Request"
+                        let key = "ErrorCode.\(self).LocalizedDescription.Login"
+                        let value = "Username or password is incorrect"
+                        
+                        let customError = NSError(domain: CorePedidoClientErrorDomain, code: errorCode!.rawValue, userInfo: [NSLocalizedDescriptionKey: NSLocalizedString(key, tableName: tableName, bundle: frameworkBundle!, value: value, comment: comment)])
+                        
+                        completionBlock(error: customError, token: nil)
+                        
+                        return
+                    }
+                    
+                    completionBlock(error: errorCode!.toError(), token: nil)
+                    
+                    return
+                }
+                
+                // no recognizeable error code
+                completionBlock(error: ErrorCode.InvalidServerResponse.toError(), token: nil)
+                
+                return
+            }
+            
+            // parse response...
+            
+            let jsonObject = NSJSONSerialization.JSONObjectWithData(request.HTTPBody!, options: NSJSONReadingOptions.allZeros, error: nil) as? [String: String]
+            
+            let token = jsonObject?["token"]
+            
+            // invalid JSON response
+            if token == nil {
+                
+                completionBlock(error: ErrorCode.InvalidServerResponse.toError(), token: nil)
+                
+                return
+            }
+            
+            // tell delegate
+            self.delegate?.store(self, didLoginWithUsername: username, password: password, token: token!)
+            
+            // completion block
+            completionBlock(error: nil, token: token)
         })
+        
+        dataTask.resume()
+        
+        return dataTask
     }
 }
 
