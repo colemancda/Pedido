78056671b047f4c0d47f083f02f4abc7f0b687df
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-12-13 13:38:41 -0500

    working on CurrencyLocalePickerViewController

--- Pedido Admin/Pedido Admin/CurrencyLocalePickerViewController.swift
+++ Pedido Admin/Pedido Admin/CurrencyLocalePickerViewController.swift
@@ -13,6 +13,50 @@ class CurrencyLocalePickerViewController: UITableViewController {
     
     // MARK: - Properties
     
+    /** Currency Locales sorted by ISO code. */
+    class var currencyLocales: [NSLocale] {
+        struct Static {
+            static var onceToken : dispatch_once_t = 0
+            static var instance : [NSLocale]? = nil
+        }
+        dispatch_once(&Static.onceToken) {
+            
+            let currencylocales: [NSLocale] = {
+                
+                let localeIdentifiers = NSLocale.availableLocaleIdentifiers() as [String]
+                
+                // create locales
+                var locales = [NSLocale]()
+                
+                for localeID in localeIdentifiers {
+                    
+                    let locale = NSLocale(localeIdentifier: localeID)
+                    
+                    if let currencyCode = locale.objectForKey(NSLocaleCurrencyCode) as? String {
+                        
+                        locales.append(NSLocale(localeIdentifier: localeID))
+                    }
+                }
+                
+                // sort by currency symbol
+                let sortedLocales = (locales as NSArray).sortedArrayUsingComparator({ (first, second) -> NSComparisonResult in
+                    
+                    let firstCurrencyCode = (first as NSLocale).objectForKey(NSLocaleCurrencyCode) as String
+                    
+                    let secondCurrencyCode = (second as NSLocale).objectForKey(NSLocaleCurrencyCode) as String
+                    
+                    return (firstCurrencyCode as NSString).compare(secondCurrencyCode)
+                }) as [NSLocale]
+                
+                return sortedLocales
+                }()
+            
+            Static.instance = currencylocales
+        }
+        return Static.instance!
+    }
+    
+    /** The selected currency locale. */
     var selectedCurrencyLocale: NSLocale? {
         
         didSet {
@@ -23,33 +67,16 @@ class CurrencyLocalePickerViewController: UITableViewController {
     
     // MARK: - Private Properties
     
-    private let localeIdentifiers = (NSLocale.availableLocaleIdentifiers() as NSArray).sortedArrayUsingSelector("localizedCaseInsensitiveCompare")
+    private let numberFormatter: NSNumberFormatter = {
+        
+        let numberFormatter = NSNumberFormatter()
+        
+        numberFormatter.numberStyle = .CurrencyStyle
+        
+        return numberFormatter
+    }()
     
-    private let locales: [NSLocale] = {
-        
-        let localeIdentifiers = NSLocale.availableLocaleIdentifiers() as [String]
-        
-        // create locales
-        var locales = [NSLocale]()
-        
-        for localeID in localeIdentifiers {
-            
-            locales.append(NSLocale(localeIdentifier: localeID))
-        }
-        
-        // sort by currency symbol
-        let sortedLocales = (locales as NSArray).sortedArrayUsingComparator({ (first, second) -> NSComparisonResult in
-            
-            let firstCurrencyCode = (first as NSLocale).objectForKey(NSLocaleCurrencyCode) as String
-            
-            let secondCurrencyCode = (second as NSLocale).objectForKey(NSLocaleCurrencyCode) as String
-            
-            return (firstCurrencyCode as NSString).compare(secondCurrencyCode)
-        }) as [NSLocale]
-        
-        return sortedLocales
-        }()
-
+    // MARK: - Initialization
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,5 +87,45 @@ class CurrencyLocalePickerViewController: UITableViewController {
         
     }
     
+    // MARK: - UITableViewDataSource
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.dynamicType.currencyLocales.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier.CurrencyLocaleCell.rawValue, forIndexPath: indexPath) as UITableViewCell
+        
+        self.configureCell(cell, atIndexPath: indexPath)
+        
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    // MARK: - Private Methods
+    
+    private func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+        
+        // get model
+        
+        let locale = self.dynamicType.currencyLocales[indexPath.row]
+        
+        
+    }
 }
+
+// MARK: - Private Enumerations
+
+private enum CellIdentifier: String {
+    
+    case CurrencyLocaleCell = "CurrencyLocaleCell"
+}
+
